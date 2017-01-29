b81241d88956570b74aaaf617fa06a89f1f1844e
Adil Sadik <sparky.005@gmail.com>
2017-01-29 18:10:57 -0500

    Get general user information

---
+++ twitalytics/modules.py
@@ -0,0 +1,52 @@
+from .api import *
+import sys
+import locale
+
+def get_general(users, api):
+    """Get general information about Twitter user"""
+
+    # set locale
+    locale.setlocale(locale.LC_ALL, '')
+
+    for handle in users:
+        # get information from api
+        try:
+            user = api.get_user(handle)
+            timeline = api.user_timeline(handle, count=3)
+        except tweepy.TweepError:
+            print("Error: Couldn't query tweepy API. Quitting!")
+            sys.exit(1)
+
+        # get human-readable forms of potentially large numbers
+        followers_count = (locale.format('%d', int(user.followers_count), grouping=True))
+        friends_count = str(locale.format('%d', int(user.friends_count), grouping=True))
+        
+        # print information
+        print("Information about %s: " % handle)
+        print("    Real name: %s" % user.name)
+        print("    Description: %s" % user.description)
+        print("    Website: %s" % user.url)
+        print("    Location: %s" % user.location)
+        print("    Followers: %s" % followers_count)
+        print("    Following: %s" % friends_count)
+        print("    Last three tweets: ")
+        for tweet in timeline:
+            print("     %s" % tweet.text)
+            print('\t    via %s' % tweet.source)
+        print()
+
+def get_devices():
+    """Get top devices that user tweets from"""
+    pass
+
+def get_locations():
+    """Get top locations that user tweets from"""
+    pass
+
+def get_topics():
+    """Get top topics the user tweets about"""
+    pass
+
+def get_twp():
+    """Get tweets per day"""
+    pass
