045b9e966fd78e4e503ac97de1fb326979574ff9
Adil Sadik <sparky.005@gmail.com>
2017-01-29 19:37:27 -0500

    Get more than 200 tweets; refactor

--- twitalytics/modules.py
+++ twitalytics/modules.py
@@ -1,39 +1,22 @@
 from .api import *
+from .module_functions import *
 import sys
-import locale
 
 def get_general(users, count, api):
     """Get general information about Twitter user"""
 
-    # set locale
-    locale.setlocale(locale.LC_ALL, '')
 
     for handle in users:
         # get information from api
         try:
             user = api.get_user(handle)
-            timeline = api.user_timeline(handle, count=count)
+            timeline = get_timeline(handle, count, api)
         except tweepy.TweepError:
             print("Error: Couldn't query tweepy API. Quitting!")
             sys.exit(1)
 
-        # get human-readable forms of potentially large numbers
-        followers_count = locale.format('%d', int(user.followers_count), grouping=True)
-        friends_count = locale.format('%d', int(user.friends_count), grouping=True)
-        
-        # print information
-        print("Information about %s: " % handle)
-        print("    Real name: %s" % user.name)
-        print("    Description: %s" % user.description)
-        print("    Website: %s" % user.url)
-        print("    Location: %s" % user.location)
-        print("    Followers: %s" % followers_count)
-        print("    Following: %s" % friends_count)
-        print("    Last %d tweets: " % count)
-        for tweet in timeline:
-            print("     %s" % tweet.text)
-            print('\t    via %s' % tweet.source)
-        print()
+        print_general(user, timeline, count)
+
 
 def get_devices():
     """Get top devices that user tweets from"""
