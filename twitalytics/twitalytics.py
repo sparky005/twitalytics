e3927d6f183f2cb2c73eafa46de34cd1b4711203
Adil Sadik <sparky.005@gmail.com>
2017-01-30 19:03:06 -0500

    Refactor argparse

--- twitalytics/twitalytics.py
+++ twitalytics/twitalytics.py
@@ -6,10 +6,34 @@ from .api import *
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--test", action='store_true', help="This is a test")
-    parser.add_argument("-u", "--users", "--user", nargs='*', metavar="username", help="User(s) to get information about.")
-    parser.add_argument("-c", "--count", type=int, default=3, metavar="int", help="Number of tweets to fetch.")
-    parser.add_argument("-p", "--print", action='store_true', help="Print gathered tweets")
+    parser.add_argument(
+        "-t", "--test",
+        action='store_true',
+        help="This is a test"
+    )
+    parser.add_argument(
+        "-u",
+        "--users",
+        "--user",
+        nargs='*',
+        metavar="username",
+        required=True,
+        help="User(s) to get information about."
+    )
+    parser.add_argument(
+        "-c",
+        "--count",
+        type=int,
+        default=100,
+        metavar="int",
+        help="Number of tweets to fetch."
+    )
+    parser.add_argument(
+        "-p",
+        "--print",
+        action='store_true',
+        help="Print gathered tweets"
+    )
     if len(sys.argv)==1:
         parser.print_help()
         sys.exit(1)
