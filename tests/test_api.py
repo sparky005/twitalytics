f327ad81e3982ddad2887cf16313d6f4382aa28c
Adil Sadik <sparky.005@gmail.com>
2017-08-20 22:25:14 -0400

    Use nytimes

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,8 +9,8 @@ def client_api():
     return client_api
 
 @vcr.use_cassette('tests/vcr_cassettes/timeline.yml')
-def test_get_timeline(capsys, client_api):
+def test_get_timeline(client_api):
     start_date = datetime.datetime.strptime('2016-01-01', '%Y-%m-%d')
-    end_date = datetime.datetime.strptime('2017-08-20', '%Y-%m-%d')
-    timeline = api.get_timeline('cnn', 100, client_api, start_date, end_date)
+    end_date = datetime.datetime.strptime('2017-08-22', '%Y-%m-%d')
+    timeline = api.get_timeline('nytimes', 100, client_api, start_date, end_date)
     assert len(timeline) == 100, "Should get 100 tweets"
