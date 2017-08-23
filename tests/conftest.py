591ba20ac64c82dbe7be8f63e6b4ac4cd7b2c796
Adil Sadik <sparky.005@gmail.com>
2017-08-22 20:08:38 -0400

    Move fixtures to conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+import vcr
+from twitalytics import api
+import datetime
+
+@pytest.fixture
+@vcr.use_cassette('tests/vcr_cassettes/timeline.yml')
+def timeline():
+    client_api = api.get_api()
+    start_date = datetime.datetime.strptime('2016-01-01', '%Y-%m-%d')
+    end_date = datetime.datetime.strptime('2017-08-22', '%Y-%m-%d')
+    timeline = api.get_timeline('nytimes', 100, client_api, start_date, end_date)
+    return timeline
+
+@pytest.fixture
+@vcr.use_cassette('tests/vcr_cassettes/user.yml')
+def user():
+    return client_api.get_user('nytimes')
+
+@pytest.fixture
+def client_api():
+    client_api = api.get_api()
+    return client_api
+
+@pytest.fixture
+def user_info():
+    return """Information about nytimes:
+    Real name: The New York Times
+    Description: Where the conversation begins. Follow for breaking news, special reports, RTs of our journalists and more from https://t.co/YapuoqX0HS.
+    Website: http://t.co/ahvuWqicF9
+    Location: New York City
+    Followers: 39,082,046
+    Following: 883
+    """
--- tests/test_modules.py
+++ tests/test_modules.py
@@ -1,43 +1,14 @@
-from twitalytics import modules, api
-import pytest
+from twitalytics import modules
 import vcr
 import tweepy
-import datetime
 from collections import Counter
 import sklearn
 import textblob
 
-@pytest.fixture
-@vcr.use_cassette('tests/vcr_cassettes/timeline.yml')
-def timeline():
-    client_api = api.get_api()
-    start_date = datetime.datetime.strptime('2016-01-01', '%Y-%m-%d')
-    end_date = datetime.datetime.strptime('2017-08-22', '%Y-%m-%d')
-    timeline = api.get_timeline('nytimes', 100, client_api, start_date, end_date)
-    return timeline
-
-@pytest.fixture
 @vcr.use_cassette('tests/vcr_cassettes/user.yml')
-def user():
-    return client_api.get_user('nytimes')
-
-@pytest.fixture
-def client_api():
-    client_api = api.get_api()
-    return client_api
-
-
-@vcr.use_cassette('tests/vcr_cassettes/user.yml')
-def test_get_user(client_api, capsys):
+def test_get_user(client_api, capsys, user_info):
     user = client_api.get_user('nytimes')
-    desired_output = """Information about nytimes:
-    Real name: The New York Times
-    Description: Where the conversation begins. Follow for breaking news, special reports, RTs of our journalists and more from https://t.co/YapuoqX0HS.
-    Website: http://t.co/ahvuWqicF9
-    Location: New York City
-    Followers: 39,082,046
-    Following: 883
-    """
+    desired_output = user_info
     desired_output = desired_output.strip()
 
     assert isinstance(user, tweepy.models.User)
